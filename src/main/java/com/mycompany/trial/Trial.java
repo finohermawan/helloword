2eb24f92b0328657a4400a40257bbd93918da077
finohermawan <finopenulisartikel@gmail.com>
2022-10-11 11:20:20 +0700

    trial

---
+++ src/main/java/com/mycompany/trial/Trial.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.trial;
+
+/**
+ *
+ * @author Asus
+ */
+public class Trial {
+
+    public static void main(String[] args) {
+        System.out.println("saya");
+        System.out.println("saya");
+        System.out.println("disini");
+        System.out.println("aliefino");
+    }
+}
